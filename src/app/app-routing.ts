c7f1c07f240e439f9655a328359095496ab1ad67
AndieAppBuilder <96213792+AndieAppBuilder@users.noreply.github.com>
2022-11-07 15:36:31 -0300

    CRM - Final initial version

---
+++ src/app/app-routing.ts
@@ -0,0 +1,18 @@
+import { Route } from '@vaadin/router';
+import './not-found/not-found.js';
+import './home/home';
+import './accounts/accounts';
+import './account-sample/account-sample';
+import './contacts/contacts';
+import './contracts/contracts';
+
+export const routes: Route[] = [
+  { path: '', component: 'app-home', name: 'Home' },
+  { path: 'home', component: 'app-home', name: 'Home' },
+  { path: 'accounts', component: 'app-accounts', name: 'Accounts' },
+  { path: 'account-sample', component: 'app-account-sample', name: 'Account Sample' },
+  { path: 'contacts', component: 'app-contacts', name: 'Contacts' },
+  { path: 'contracts', component: 'app-contracts', name: 'Contracts' },
+  // The fallback route should always be after other alternatives.
+  { path: '(.*)', component: 'app-not-found' }
+];
