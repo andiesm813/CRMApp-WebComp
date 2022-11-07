26092b685a7eb7eee93091b1502cd76b15408913
AndieAppBuilder <96213792+AndieAppBuilder@users.noreply.github.com>
2022-11-07 19:59:43 -0300

    CRM - Final updates

--- src/app/account-sample/account-sample.ts
+++ src/app/account-sample/account-sample.ts
@@ -1,8 +1,8 @@
 import { html, css, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { defineComponents, IgcButtonComponent, IgcIconComponent, IgcRippleComponent } from 'igniteui-webcomponents';
+import { defineComponents, IgcButtonComponent, IgcExpansionPanelComponent, IgcIconComponent, IgcRippleComponent, IgcTabsComponent } from 'igniteui-webcomponents';
 
-defineComponents(IgcButtonComponent, IgcRippleComponent, IgcIconComponent);
+defineComponents(IgcTabsComponent, IgcButtonComponent, IgcIconComponent, IgcRippleComponent, IgcExpansionPanelComponent);
 
 @customElement('app-account-sample')
 export default class AccountSample extends LitElement {
@@ -10,6 +10,7 @@ export default class AccountSample extends LitElement {
     :host {
       height: 100%;
       display: flex;
+      flex-direction: column;
       justify-content: flex-start;
       align-items: stretch;
       align-content: flex-start;
@@ -19,10 +20,144 @@ export default class AccountSample extends LitElement {
       flex-direction: column;
     }
     .group {
+      border-color: hsla(var(--ig-gray-200));
+      border-width: 0px 0px 2px;
+      border-style: solid;
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: space-between;
+      position: relative;
+      padding: 20px;
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .row-layout {
+      display: flex;
+    }
+    .account-header {
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: flex-start;
+      align-content: flex-start;
+      gap: 16px;
+      position: relative;
+      min-width: 50px;
+    }
+    .page-title {
+      justify-content: flex-start;
+      align-items: center;
+      align-content: flex-start;
+      gap: 16px;
+      position: relative;
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .group_1 {
+      background-color: rgba(71, 124, 41, 0.2);
+      justify-content: center;
+      align-items: center;
+      align-content: flex-start;
+      position: relative;
+      width: 48px;
+      height: 48px;
+    }
+    .group_2 {
+      justify-content: center;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .group_3 {
+      flex-wrap: wrap;
       justify-content: flex-start;
       align-items: stretch;
       align-content: flex-start;
-      gap: 16px;
+      gap: 24px;
+      position: relative;
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .group_4 {
+      border-color: hsla(var(--ig-gray-200));
+      border-width: 0px 1px 0px 0px;
+      border-style: solid;
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      padding: 0 24px 0 0;
+      min-width: 50px;
+      min-height: 50px;
+      flex-shrink: 0;
+    }
+    .group_5 {
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      min-width: 50px;
+    }
+    .group_6 {
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      min-width: 50px;
+      min-height: 50px;
+      flex-shrink: 0;
+    }
+    .group_7 {
+      background-color: hsla(var(--ig-gray-100));
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      gap: 12px;
+      position: relative;
+      padding: 12px;
+      min-width: 50px;
+      min-height: 50px;
+      flex-grow: 1;
+      flex-basis: 0;
+    }
+    .group_8 {
+      background-color: hsla(var(--ig-surface-500));
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      padding: 16px;
+      min-width: 50px;
+      min-height: 50px;
+      flex-grow: 2;
+      flex-basis: 0;
+    }
+    .tabs {
+      min-width: 0;
+      min-height: 0;
+      flex-grow: 1;
+      flex-basis: 0;
+    }
+    .group_9 {
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      position: relative;
+      min-width: 50px;
+      min-height: 50px;
+      flex-grow: 1;
+      flex-basis: 0;
+    }
+    .accordion {
+      overflow-y: auto;
+      flex-shrink: 0;
+    }
+    .group_10 {
+      background-color: hsla(var(--ig-surface-500));
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
       position: relative;
       padding: 16px;
       min-width: 50px;
@@ -30,20 +165,41 @@ export default class AccountSample extends LitElement {
       flex-grow: 1;
       flex-basis: 0;
     }
-    .group_1 {
+    .image {
+      object-fit: cover;
+      width: 20px;
+      height: 20px;
+      min-height: 0;
+      flex-shrink: 0;
+    }
+    .content {
+      height: max-content;
+      min-width: min-content;
+    }
+    .text {
+      color: hsla(var(--ig-gray-700));
+      height: max-content;
+      min-width: min-content;
+    }
+    .text_1 {
+      color: hsla(var(--ig-primary-500));
+      height: max-content;
+      min-width: min-content;
+    }
+    .tab-item-content {
       justify-content: flex-start;
       align-items: stretch;
       align-content: flex-start;
-      position: relative;
-      padding: 16px;
-      min-width: 50px;
+      overflow: auto;
+      position: absolute;
+      height: 100%;
+      min-width: 100%;
       min-height: 50px;
-      flex-grow: 1;
-      flex-basis: 0;
     }
     .button {
       height: max-content;
       min-width: min-content;
+      flex-shrink: 0;
     }
   `;
 
@@ -53,41 +209,206 @@ export default class AccountSample extends LitElement {
       <link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet'>
       <link rel='stylesheet' href='../../ig-theme.css'>
       <div class="column-layout group">
-        <igc-button class="button">
-          Button
-          <igc-ripple></igc-ripple>
-        </igc-button>
-        <igc-button class="button">
-          <span class="material-icons">
-            add
-          </span>
-          <span>Button</span>
-          <igc-ripple></igc-ripple>
-        </igc-button>
-        <igc-button variant="outlined" class="button">
-          Button
-          <igc-ripple></igc-ripple>
-        </igc-button>
-        <igc-button variant="outlined" class="button">
-          <span class="material-icons">
-            add
-          </span>
-          <span>Button</span>
-          <igc-ripple></igc-ripple>
-        </igc-button>
-        <igc-button variant="flat" class="button">
-          Button
-          <igc-ripple></igc-ripple>
-        </igc-button>
-        <igc-button variant="flat" class="button">
-          <span class="material-icons">
-            add
-          </span>
-          <span>Button</span>
-          <igc-ripple></igc-ripple>
-        </igc-button>
+        <div class="row-layout account-header">
+          <div class="row-layout page-title">
+            <div class="row-layout group_1">
+              <img src="/src/assets/Accounts Icon - Green.svg" class="image" />
+            </div>
+            <div class="column-layout group_2">
+              <h6 class="content">
+                Around the Horn
+              </h6>
+              <p class="typography__body-2 text">
+                Brewing Company
+              </p>
+            </div>
+          </div>
+          <div class="row-layout group_3">
+            <div class="column-layout group_4">
+              <p class="typography__subtitle-2 content">
+                Primary Contact
+              </p>
+              <p class="typography__caption text">
+                Thomas Hardy
+              </p>
+              <p class="typography__caption text">
+                Sales Representative
+              </p>
+            </div>
+            <div class="column-layout group_4">
+              <p class="typography__subtitle-2 content">
+                Phone
+              </p>
+              <p class="typography__caption text">
+                (123) 456-7890
+              </p>
+            </div>
+            <div class="column-layout group_4">
+              <p class="typography__subtitle-2 content">
+                Billing Address
+              </p>
+              <p class="typography__caption text">
+                120 Hanover Sq.
+              </p>
+              <div class="column-layout group_5">
+                <p class="typography__caption text">
+                  London,  WA1 1DP
+                </p>
+                <p class="typography__caption text">
+                  UK
+                </p>
+              </div>
+            </div>
+            <div class="column-layout group_4">
+              <p class="typography__subtitle-2 content">
+                Website
+              </p>
+              <p class="typography__caption text_1">
+                www.aroundthehorn.com
+              </p>
+            </div>
+            <div class="column-layout group_6">
+              <p class="typography__subtitle-2 content">
+                Account Owner
+              </p>
+              <p class="typography__caption text">
+                Sandy Anderson
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="column-layout group_1"></div>
+      <div class="row-layout group_7">
+        <div class="column-layout group_8">
+          <igc-tabs class="tabs">
+            <igc-tab-panel class="row-layout tab-item-content">
+              <div class="column-layout group_9">
+                <div class="row-layout page-title">
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                </div>
+                <igc-accordion ?single-expand="${false}" class="accordion">
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                </igc-accordion>
+              </div>
+            </igc-tab-panel>
+            <igc-tab ?selected="${true}">
+              <span class="material-icons">info</span>
+              Label
+            </igc-tab>
+            <igc-tab-panel class="row-layout tab-item-content"></igc-tab-panel>
+            <igc-tab ?disabled="${true}">
+              <span class="material-icons">info</span>
+              Label
+            </igc-tab>
+            <igc-tab-panel class="row-layout tab-item-content">
+              <div class="column-layout group_9"></div>
+            </igc-tab-panel>
+            <igc-tab ?disabled="${true}">
+              <span class="material-icons">info</span>
+              Label
+            </igc-tab>
+          </igc-tabs>
+        </div>
+        <div class="column-layout group_10">
+          <igc-tabs class="tabs">
+            <igc-tab-panel class="row-layout tab-item-content">
+              <div class="column-layout group_9">
+                <div class="row-layout page-title">
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                  <igc-button class="button">
+                    <span class="material-icons">
+                      add
+                    </span>
+                    <span>Button</span>
+                    <igc-ripple></igc-ripple>
+                  </igc-button>
+                </div>
+                <igc-accordion ?single-expand="${false}" class="accordion">
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                  <igc-expansion-panel>
+                    <p class="typography__body-1 content"></p>
+                    <span slot="title">Title goes here...</span>
+                  </igc-expansion-panel>
+                </igc-accordion>
+              </div>
+            </igc-tab-panel>
+            <igc-tab ?selected="${true}">
+              <span class="material-icons">info</span>
+              Label
+            </igc-tab>
+            <igc-tab-panel class="row-layout tab-item-content"></igc-tab-panel>
+            <igc-tab ?disabled="${true}">
+              <span class="material-icons">info</span>
+              Label
+            </igc-tab>
+          </igc-tabs>
+        </div>
+      </div>
     `;
   }
 }
